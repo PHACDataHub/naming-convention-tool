fbc1978dc4a6eddce50b1c6313ea5b55916f37aa
Sami Atoui <63279480+samiatoui@users.noreply.github.com>
2023-03-09 18:13:54 +0000

    format fix

--- cloudnaming/src/App.vue
+++ cloudnaming/src/App.vue
@@ -1,27 +1,27 @@
 <script setup>
-import Naming from "./components/Naming.vue"
+import Naming from './components/Naming.vue'
 </script>
 
 <template>
-  <div class="header">
-    <h1>Cloud Naming Convention</h1>
-  </div>
-  <div class="main">
-    <Naming />
-
+  <div>
+    <div class="header">
+      <h1>Cloud Naming Convention</h1>
+    </div>
+    <div class="main">
+      <Naming />
+    </div>
   </div>
 </template>
 
 <style scoped>
-.header{
+.header {
   padding: 5px 20px;
   background-color: rgb(252, 48, 82);
   color: white;
   border-radius: 5px;
-
 }
 
-.main{
+.main {
   width: 100%;
   margin-top: 25px;
 }
